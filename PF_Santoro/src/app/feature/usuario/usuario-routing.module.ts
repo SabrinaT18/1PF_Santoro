e64a4d354472c0f4beaa8199d4c04f0f9ad1c469
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-09-06 22:22:48 -0300

    ruta en toolbar

--- PF_Santoro/src/app/feature/usuario/usuario-routing.module.ts
+++ PF_Santoro/src/app/feature/usuario/usuario-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ClasesComponent } from "../componentes/clases/clases.component";
-import { CursosComponent } from "../componentes/cursos/cursos.component";
-import { InscripcionesComponent } from "../componentes/inscripciones/inscripciones.component";
-import { ListaAlumnosComponent } from "../componentes/lista-alumnos/lista-alumnos.component";
 import { UsuarioComponent } from "./usuario.component";
 
 const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent, 
-  children: [
-    {path: 'alumnos', component: ListaAlumnosComponent},
-    {path: 'cursos', component: CursosComponent} ,
-    {path: 'inscripciones', component: InscripcionesComponent},
-    {path: 'clases', component: ClasesComponent},
-  ]},
+  },
     { path: '**', redirectTo: 'Inicio', pathMatch: 'full' },
 ];
 
